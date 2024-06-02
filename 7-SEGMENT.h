f9c4ae5842a7ff84581be19b6ac6547f9568f14e
Reem Raouf <reemraouf299@gmail.com>
2024-06-02 15:42:48 +0300

    Add files via upload

---
+++ 7-SEGMENT.h
@@ -0,0 +1,13 @@
+/*
+ * _7_SEGMENT.h
+ *
+ * Created: 6/2/2024 3:39:49 PM
+ *  Author: Reem Raouf
+ */ 
+
+
+#ifndef 7-SEGMENT_H_
+#define 7-SEGMENT_H_
+void SevenSegment_Display(uint8 num)
+
+#endif /* 7-SEGMENT_H_ */
